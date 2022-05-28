5f2079ba5b0cc7c8d6423a576b2689d0cbd57a81
lkiera-Lee <85823496+lkiera-Lee@users.noreply.github.com>
2022-05-28 13:45:23 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '10fcc8bfeca844c2a929bb28edc3b2da?v=1e684fe381944e75be48fb84afd599ee',
+  rootNotionPageId: '4d31ffbb17254f9d9270ead8435b827d?v=8584e481e1de4408ab349a8971805720',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Keep Fun',
-  domain: 'https://www.notion.so/10fcc8bfeca844c2a929bb28edc3b2da?v=1e684fe381944e75be48fb84afd599ee',
+  domain: 'https://www.notion.so/4d31ffbb17254f9d9270ead8435b827d?v=8584e481e1de4408ab349a8971805720',
   author: 'lkiera',
 
   // open graph metadata (optional)
